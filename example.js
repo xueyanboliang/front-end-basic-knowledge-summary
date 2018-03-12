91e9427cef96dc51b5037e478a25db69e2b116b0
ubuntu <18751803026@163.com>
2018-03-12 16:47:22 +0800

    继续更新中

--- example.js
+++ example.js
@@ -1,1 +1,18 @@
-console.log(Math.max(1, 3, 0,23, 8, 9))
+function quickSort(arr) {
+    if (arr.length < 2) return arr
+    let arrLeft = []
+    let arrRight = []
+    let center = []
+    let centerNumber = arr[Math.floor((arr.length - 1)/2)]
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < centerNumber) {
+            arrLeft.push(arr[i])
+        } else if (arr[i] > centerNumber) {
+            arrRight.push(arr[i])
+        } else {
+            center.push(arr[i])
+        }
+    }
+    return quickSort(arrLeft).concat(center,quickSort(arrRight))
+}
+console.log(quickSort( [3, 1, 4, 6, 5, 7, 2]))
